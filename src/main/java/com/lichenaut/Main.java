b443c87a93fc97d9428fed273c0bc8130ebf7847
Matthew <mt0n@tutanota.com>
2024-10-23 19:01:25 -0600

    env var switch

--- src/main/java/com/lichenaut/Main.java
+++ src/main/java/com/lichenaut/Main.java
@@ -3,7 +3,6 @@ package com.lichenaut;
 import com.lichenaut.listen.MessageListener;
 import com.lichenaut.util.MessageScanner;
 import com.lichenaut.util.MessageSender;
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -11,7 +10,10 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 @Getter
 public class Main {
     public static void main(String[] args) {
-        JDABuilder.createLight(Dotenv.load().get("BOT_TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
+//        JDABuilder.createLight(Dotenv.load().get("BOT_TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
+//                .addEventListeners(new MessageListener(new MessageScanner(), new MessageSender()))
+//                .build();
+        JDABuilder.createLight(System.getenv("BOT_TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new MessageListener(new MessageScanner(), new MessageSender()))
                 .build();
     }

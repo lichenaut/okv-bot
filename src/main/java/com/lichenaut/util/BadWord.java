6a45df90a640debb4c780eb01cdcaa266cdb3b36
Matthew <mt0n@tutanota.com>
2024-11-24 15:37:17 -0700

    2 new words

--- src/main/java/com/lichenaut/util/BadWord.java
+++ src/main/java/com/lichenaut/util/BadWord.java
@@ -15,17 +15,19 @@ public enum BadWord {
     SUB_3("degenera"),
     SUB_4("dumb"),
     SUB_5("fool"),
-    SUB_6("idiot"),
-    SUB_7("imbecile"),
-    SUB_8("insane"),
-    SUB_9("moron"),
-    SUB_10("reeeee"),
-    SUB_11("retard"),
-    SUB_12("scum"),
-    SUB_13("sociopath"),
-    SUB_14("spastic"),
-    SUB_15("spaz"),
-    SUB_16("stupid");
+    SUB_6("freak"),
+    SUB_7("hysteri"),
+    SUB_8("idiot"),
+    SUB_9("imbecile"),
+    SUB_10("insane"),
+    SUB_11("moron"),
+    SUB_12("reeeee"),
+    SUB_13("retard"),
+    SUB_14("scum"),
+    SUB_15("sociopath"),
+    SUB_16("spastic"),
+    SUB_17("spaz"),
+    SUB_18("stupid");
 
     private final String word;
 
@@ -50,7 +52,7 @@ public enum BadWord {
                 SUB.word, SUB_2.word, SUB_3.word, SUB_4.word, SUB_5.word,
                 SUB_6.word, SUB_7.word, SUB_8.word, SUB_9.word,
                 SUB_10.word, SUB_11.word, SUB_12.word, SUB_13.word,
-                SUB_14.word, SUB_15.word, SUB_16.word
+                SUB_14.word, SUB_15.word, SUB_16.word, SUB_17.word, SUB_18.word
         };
     }
 }
